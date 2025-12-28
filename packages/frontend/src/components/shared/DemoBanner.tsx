869dc4f126526cde893c921d5aade8d966cbb289
Andri Purnomo <andri.adrp@gmail.com>
2025-12-28 19:58:13 +0700

    fix: demo banner and env value

--- packages/frontend/src/components/shared/DemoBanner.tsx
+++ packages/frontend/src/components/shared/DemoBanner.tsx
@@ -18,7 +18,7 @@ export const DemoBanner = () => {
               d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
             />
           </svg>
-          <p className="text-sm text-yellow-800 dark:text-yellow-200">
+          <p className="text-sm text-yellow-800 dark:text-black">
             <span className="font-semibold">Demo Account:</span> Your data will be deleted after 24
             hours of inactivity.
           </p>
